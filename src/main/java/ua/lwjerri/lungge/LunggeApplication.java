53e5037ba7e25af175c13f52c2a0ef0e26dd01d0
Andrii Zontov <loginmanx@gmail.com>
2025-06-05 11:42:47 +0200

    chore: Add main app file

---
+++ src/main/java/ua/lwjerri/lungge/LunggeApplication.java
@@ -0,0 +1,13 @@
+package ua.lwjerri.lungge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LunggeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LunggeApplication.class, args);
+    }
+
+}
